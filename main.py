9055b51588be7e049eeb20849d89b124bed5752d
Erick Almeida <erickalmeiida01@gmail.com>
2026-06-14 20:44:16 -0300

    Adicionando imagem ao projeto

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import customtkinter as ctk
+from tkinter import*
 
 
 class App(ctk.CTk):
      def __init__(self):
            super ().__init__() 
            self.configuracao_da_janela_inicial()
+           self.tela_de_login()
 
    #Configurando a janela principal 
      def configuracao_da_janela_inicial(self):
         self.geometry("700x400")
         self.title("Sistema de Login")
         self.resizable(False,False) 
+        ctk.set_appearance_mode("Dark")
 
 
-
-if __name__=="__main__":
-    app = App()
-    app.mainloop()  
+     def tela_de_login(self):
+        #Trabalhando com imagens 
