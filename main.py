08c47ed71ab32acb12f4713576eefdf620464aac
Erick Almeida <erickalmeiida01@gmail.com>
2026-08-02 19:47:55 -0300

    manin

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import customtkinter as ctk
 from PIL import Image
 import os
-print("Pasta atual do script:", os.path.dirname(__file__))
-print("Arquivos encontrados:", os.listdir(os.path.dirname(__file__)))
-
 
 class App(ctk.CTk):
      def __init__(self):
@@ -24,15 +21,90 @@ class App(ctk.CTk):
 
         self.img = ctk.CTkImage(light_image=Image.open("login-img.png"), 
                                 dark_image=Image.open("login-img.png"), 
-                                size=(250, 250))
+                                size=(280, 280))
                            
         self.lb_img = ctk.CTkLabel(self, text=None, image=self.img)
-        self.lb_img.grid(row=1, column=0, padx=10, pady=20)
+        self.lb_img.grid(row=1, column=0, padx=10,)
+        
 
-        self.title = ctk.CTkLabel(self, text="Faça seu loguin ou cadastre-se na plataforma", font=("Century Gotic bold", 14))
-        self.title.grid(row=0, column=0, pady=0)
+        #titulo da plataforma
+        self.title = ctk.CTkLabel(self, text="Faça seu loguin ou cadastre-se\n na plataforma para acessar\n nossos serviços", font=("Century Gotic bold", 14))
+        self.title.grid(row=0, column=0, pady=10, padx=10)
+
+        #criando a frame do formulario de login
+
+        self.frame_login = ctk.CTkFrame(self, width=350, height=350)
+        self.frame_login.place(x=350, y=10)   
+
+        #colocando widgets dentro do frame
+        self.lb_title = ctk.CTkLabel(self.frame_login, text="Faça o seu login", 
+        font=("Century Gothic bold", 22))
+        self.lb_title.grid(row=0, column=0, padx=10,pady=10)
+
+        self.username_login_entry = ctk.CTkEntry(self.frame_login, width=300, 
+        placeholder_text='Seu nome de usuário..', 
+        font=("Century Gothic bold", 16), corner_radius=15)
+        self.username_login_entry.grid(row=1, column=0, pady=10, padx=10)
+
+
+        self.senha_login_entry = ctk.CTkEntry(self.frame_login, width=300, 
+        placeholder_text='Seu nome de usuário..', 
+        font=("Century Gothic bold", 16), corner_radius=15, show='*')
+        self.senha_login_entry.grid(row=2, column=0, pady=10, padx=10)
+
+        self.ver_senha = ctk.CTkCheckBox(self.frame_login, 
+        text ="Clique para ver a senha", 
+        font=("Century Gothic bold", 16), corner_radius=20)
+        self.ver_senha.grid(row=3, column=0, pady=10, padx=10)
+
+        self.btn_login = ctk.CTkButton(self.frame_login, width=300, 
+        text = 'Fazer Login', 
+        font=("Century Gothic bold", 16), corner_radius=15)
+        self.btn_login.grid(row=4, column=0, pady=10, padx=10)
+
+
+       
+        self.sap = ctk.CTkLabel(self.frame_login, 
+        text= "Se não tem conta, clique aqui para se cadastrar.",
+        font=("Century Gothic", 10))
+        self.sap.grid(row=5, column=0, pady=10, padx=10)
+
+        self.btn_cadastro = ctk.CTkButton(self.frame_login, width=300, fg_color= 'green', 
+        text = 'Fazer Cadastro', 
+        font=("Century Gothic bold", 16), corner_radius=15, command = self.tela_de_cadastro)
+        self.btn_cadastro.grid(row=4, column=0, pady=10, padx=10)
+
+
+     def tela_de_cadastro(self):
+         #Remover o formulário de login
+         self.frame_login.place_forget()
+
+
+
+
+
+
+
+
+
+                                                                                                                      
+
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__=="__main__":
-     App = App()
-     App.mainloop()
+     app = App()
+     app.mainloop()
